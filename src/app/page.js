a7714e2d52d888ab9722f3c683c432c36f6eb239
gabewolford <gabewolford@gmail.com>
2023-08-03 14:47:24 -0700

    updated presented by with new props

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ import PresentedBy from "./components/PresentedBy"
 export default function HomePage() {
   return (
     <main>
-      <PresentedBy />
+      <PresentedBy color={'text-brown-500'} display={'hidden md:flex md:flex-row'}/>
       <Hero />
     </main>
   )
