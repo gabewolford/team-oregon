c1889473d162ff731d300a05047fec00d1f6d2ba
gabewolford <gabewolford@gmail.com>
2023-08-03 15:57:59 -0700

    created left content block component

---
+++ src/app/components/LeftContentBlock.jsx
@@ -0,0 +1,21 @@
+import Image from "next/image"
+import Button from "./Button"
+
+export default function LeftContentBlock({ header, text }) {
+  return (
+    <section className="border flex flex-col md:flex-row justify-center items-center gap-6 md:gap-10 mx-auto px-4 md:px-10 mb-10 md:mb-20 lg:max-w-[90vw]">
+      <div className="flex flex-col order-1 items-start gap-4 md:w-1/2">
+        <h3 className="text-[28px] font-semibold">{header}</h3>
+        <p className="leading-[160%] font-medium">{text}</p>
+        <Button text={'Meet the team'} linkTo={'/team'}/>
+      </div>
+      <Image
+        src={'/images/home-photo-right-1.png'}
+        alt="Tabor photo"
+        width={543}
+        height={518}
+        className="order-0 md:w-1/2"
+      ></Image>
+    </section>
+  )
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 import Hero from "./components/Hero"
 import PresentedBy from "./components/PresentedBy"
+import LeftContentBlock from "./components/LeftContentBlock"
 
 export default function HomePage() {
   return (
     <main>
       <PresentedBy color={'text-brown-500'} display={'hidden md:flex md:flex-row'}/>
       <Hero />
+      <LeftContentBlock header={'Team-oriented, inclusive, and supportive'} text={'Our mission is to enhance the sport of cycling in Oregon by organizing an accessible team with resources to help all members reach their goals within the sport. Experienced riders share their knowledge with less experienced team members, enabling growth and skill development.'}/>
     </main>
   )
 }
