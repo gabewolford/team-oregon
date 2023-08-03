022cfcec07cd1eba16edc3b315bf65800276b288
gabewolford <gabewolford@gmail.com>
2023-08-03 16:44:32 -0700

    added props to left and right content block components

--- src/app/components/RightContentBlock.jsx
+++ src/app/components/RightContentBlock.jsx
@@ -1,20 +1,21 @@
 import Image from "next/image"
 import Button from "./Button"
 
-export default function RightContentBlock({ header, text }) {
+export default function RightContentBlock({ header, text, buttonText, buttonLink, image, altText }) {
   return (
     <section className="flex flex-col md:flex-row justify-center items-center gap-6 md:gap-10 mx-auto px-4 md:px-10 mb-10 md:mb-20 lg:max-w-[90vw]">
       <Image
-        src={'/images/home-photo-left-1.png'}
-        alt="Tabor photo"
+        src={image}
+        alt={altText}
         width={543}
         height={785}
-        className="md:w-1/2"
-      ></Image>
+        className="md:w-1/2 w-full"
+      >
+      </Image>
       <div className="flex flex-col items-start gap-4 md:w-1/2">
         <h3 className="text-[28px] font-semibold">{header}</h3>
         <p className="leading-[160%] font-medium">{text}</p>
-        <Button text={'Get involved'} linkTo={'/join'}/>
+        <Button text={buttonText} linkTo={buttonLink}/>
       </div>
     </section>
   )
