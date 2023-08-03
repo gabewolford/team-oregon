55f92379ed9bcd7e4e2a08d2257d2270862eeb2b
gabewolford <gabewolford@gmail.com>
2023-08-03 13:07:05 -0700

    fine tuning nav

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -128,7 +128,7 @@ export default function Navbar() {
                 </div>
 
                 <div id="navbar-collapse-with-animation" className="hs-collapse hidden overflow-hidden transition-all duration-300 basis-full grow sm:block">
-                    <div className="flex flex-col gap-y-4 gap-x-0 mt-3 sm:flex-row sm:items-center sm:justify-end sm:gap-y-0 sm:gap-x-7 sm:mt-0 sm:pl-7">
+                    <div className="flex flex-col gap-y-4 gap-x-0 mt-4 sm:flex-row sm:items-center sm:justify-end sm:gap-y-0 sm:gap-x-7 sm:mt-0 sm:pl-7">
                         <Link 
                             className={`hover:text-cyan-420 sm:py-6 ${(location !== '/join' && location !== '/') && inactiveColorClass}`}
                             href="/join"
