d19da3b90aedd4ee8f2904cf5412bbd161f97f07
gabewolford <gabewolford@gmail.com>
2023-08-02 17:27:54 -0700

    updated button component with linkTo prop

--- src/app/components/Button.jsx
+++ src/app/components/Button.jsx
@@ -1,7 +1,11 @@
-export default function Button({ text }) {
+import Link from "next/link"
+
+export default function Button({ text, linkTo }) {
     return (
-        <button className="flex h-10 px-4 py-2 items-center rounded-full bg-blue-500 text-white-500 font-medium">
-            {text}
-        </button>
+        <Link href={linkTo}>
+            <button className="flex h-10 px-4 py-2 items-center rounded-full bg-blue-500 hover:bg-blue-hover text-white-500 font-medium">
+                {text}
+            </button>
+        </Link>
     )
 }
