3718d6c812d78120f1fdfc51a043086101428662
gabewolford <gabewolford@gmail.com>
2023-08-04 18:01:37 -0700

    created sponsor schema

---
+++ schemas/sponsor.js
@@ -0,0 +1,48 @@
+export default {
+    name: 'sponsor',
+    title: 'Sponsors',
+    type: 'document',
+    fields: [
+      {
+        name: 'name',
+        title: 'Name',
+        type: 'string',
+        description: 'The name of the sponsor.',
+        validation: Rule => Rule.required()
+      },
+      {
+        name: 'image',
+        title: 'Image',
+        type: 'image',
+        description: "The image of the sponsor\'s logo.",
+        validation: Rule => Rule.required()
+      },
+      {
+        name: 'website',
+        title: 'Website',
+        type: 'url',
+        description: 'The URL of the partner\'s website.',
+        validation: Rule => Rule.required()
+      },
+      {
+        name: 'facebook',
+        title: 'Facebook',
+        type: 'url',
+        description: 'The URL of the partner\'s facebook.',
+      },
+      {
+        name: 'instagram',
+        title: 'Instagram',
+        type: 'url',
+        description: 'The URL of the partner\'s instagram.',
+      },
+      {
+        name: 'description',
+        title: 'Description',
+        type: 'string',
+        description: 'A short description of the sponsor.',
+        validation: Rule => Rule.required().min(25).max(350)
+      },
+    ],
+  };
+  
