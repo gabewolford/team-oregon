2ba8bb09f5fa2ae08baa61b2ccf5c18c2d948523
gabewolford <gabewolford@gmail.com>
2023-08-04 20:24:41 -0700

    added additional sponsorship header

--- src/app/sponsors/page.jsx
+++ src/app/sponsors/page.jsx
@@ -1,9 +1,11 @@
 import GetSponsors from "../api/GetSponsors"
+import { sailorsRegular } from "../styles/fonts"
 
 export default function SponsorsPage() {
   return (
-    <main className="flex mx-10">
+    <main className="flex">
       <section className="mx-auto">
+        <h3 className={'text-center text-xl md:text-2xl text-blue-500 mb-8 font-semibold uppercase'}>Additional Sponsorship By</h3>
         <GetSponsors />
       </section>
     </main>
