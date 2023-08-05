57623f5bb6331db70a6f0b79f127faf7a109915e
gabewolford <gabewolford@gmail.com>
2023-08-04 21:39:29 -0700

    laid out sponsor components on page

--- src/app/sponsors/page.jsx
+++ src/app/sponsors/page.jsx
@@ -1,12 +1,24 @@
 import GetSponsors from "../api/GetSponsors"
-import { sailorsRegular } from "../styles/fonts"
+import CTA from "../components/CTA"
+import TitleSponsorCard from "../components/TitleSponsorCard"
 
 export default function SponsorsPage() {
   return (
-    <main className="flex">
-      <section className="mx-auto">
-        <h3 className={'text-center text-xl md:text-2xl text-blue-500 mb-8 font-semibold uppercase'}>Additional Sponsorship By</h3>
+    <main className="flex flex-col gap-10 md:gap-20">
+
+      <TitleSponsorCard />
+
+      <section className="flex flex-col gap-6 mx-auto">
+        <h3 className={'text-center text-xl md:text-2xl text-blue-500 font-semibold uppercase'}>Additional Sponsorship By</h3>
         <GetSponsors />
+      </section>
+      <section className="mt-10 mb-20">
+        <CTA
+          header={'Interest in sponsoring Team Oregon?'}
+          buttonText={'Get in touch'}
+          linkTo={'/contact'}
+        >
+        </CTA>
       </section>
     </main>
   )
