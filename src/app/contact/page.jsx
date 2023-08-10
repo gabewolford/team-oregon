b2319381f62f74a2d5a895ae8845986f92b09f77
gabewolford <gabewolford@gmail.com>
2023-08-10 12:47:18 -0700

    built contact form, configured emailjs templates

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,13 +1,20 @@
-export const metadata = {
-    title: 'Team Oregon | Contact Us',
-    description: 'Helping our members achieve their cycling goals and developing the sport of competive cycling in the Pacific Northwest for over three decades.',
-  }
+import ContactForm from "../components/ContactForm"
 
+export const metadata = {
+  title: 'Team Oregon | Contact Us',
+  description: 'Helping our members achieve their cycling goals and developing the sport of competive cycling in the Pacific Northwest for over three decades.',
+}
 
 export default function ContactPage() {
 
     return (
-        <div>Contact Page</div>
-    )
-  }
-  
+        <>
+            <main className="mx-auto max-w-7xl pt-16 md:pt-24">
+                <div className="mx-6 lg:mx-20 mb-8 md:mb-16">
+                    <h2 className='text-center text-xl md:text-3xl text-blue-500 font-semibold uppercase mb-2'>Contact Us</h2>
+                    <ContactForm />
+                </div>
+            </main>
+        </>
+    )      
+}
