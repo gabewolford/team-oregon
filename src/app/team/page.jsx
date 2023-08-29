4fd65546d1cebb4ae6be4e1d807c37c4c2bca7dd
gabewolford <gabewolford@gmail.com>
2023-08-29 16:12:45 -0700

    added boardmembers to team page

--- src/app/team/page.jsx
+++ src/app/team/page.jsx
@@ -1,3 +1,6 @@
+import CTA from "../components/CTA"
+import GetBoardmembers from "../api/boardmembers/GetBoardmembers"
+
 export const metadata = {
   title: 'Team Oregon | Meet The Team',
   description: 'Helping our members achieve their cycling goals and developing the sport of competive cycling in the Pacific Northwest for over three decades.',
@@ -5,7 +8,21 @@ export const metadata = {
 
 export default function TeamPage() {
     return (
-      <div>Team Page</div>
+      <main className="flex flex-col gap-10 md:gap-20 mt-6">
+        <section className="flex flex-col gap-6 mx-auto">
+          <h3 className={'text-center text-xl md:text-2xl text-blue-500 font-semibold uppercase'}>2023 Team Leadership</h3>
+          <GetBoardmembers />
+        </section>
+        <section className="mt-10 mb-20">
+          <CTA
+            header={"We'd love to meet you!"}
+            subtext={"Feel free to contact us with any questions."}
+            buttonText={'Get in touch'}
+            linkTo={'/contact'}
+          >
+          </CTA>
+        </section>
+      </main>
     )
   }
   
