68ef12d31711e1880cfacfc1d5e69750aa2479e8
gabewolford <gabewolford@gmail.com>
2023-09-04 10:11:49 -0700

    added button and link to pay page

--- src/app/components/AccountInfo.jsx
+++ src/app/components/AccountInfo.jsx
@@ -2,6 +2,8 @@
 
 import { signOut } from "next-auth/react";
 import { useSession } from "next-auth/react"
+import Link from "next/link";
+import Button from "./Button";
 
 export default function AccountInfo() {
     let formattedDate
@@ -17,7 +19,6 @@ export default function AccountInfo() {
         formattedDate = `${month}-${day}-${year}`;
     }
 
-
     return (
         <>
             <div className="flex flex-col gap-4 md:w-1/2 mx-auto my-10">
@@ -25,6 +26,10 @@ export default function AccountInfo() {
                 <p>Last Name: <span className="font-semibold">{session?.user?.lastName}</span></p>
                 <p>Email: <span className="font-semibold">{session?.user?.email}</span></p>
                 <p>Account created: <span className="font-semibold">{formattedDate}</span></p>
+                <p>Account status: <span className="font-semibold">Inactive</span></p>
+                <Link href="/account/pay">
+                    <Button text="Activate membership" />
+                </Link>
                 <button 
                     onClick={() => signOut()}
                     className="w-fit flex h-10 px-4 py-2 items-center rounded-full bg-red-500 hover:bg-red-hover text-white-500 font-medium"
@@ -34,6 +39,7 @@ export default function AccountInfo() {
 
             <div className="flex flex-col gap-4 md:w-1/2 mx-auto">
                 <p>Looking to order a new kit? Visit the official <span><a className="font-semibold" href="https://biciclista.us/collections/team-oregon" target="_blank">Team Store</a></span> by Biciclista!</p>
+                <p>Members-only deals from our sponsors <span><Link className="font-semibold" href="/account/sponsor-deals">HERE</Link></span>!</p>
             </div>
 
         </>
