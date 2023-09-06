618b916a164e6f56185ba198ac127b0339e87750
gabewolford <gabewolford@gmail.com>
2023-09-06 13:23:31 -0700

    added paypal button to pay page

--- src/app/account/pay/page.jsx
+++ src/app/account/pay/page.jsx
@@ -1,5 +1,10 @@
+import PayPalButton from "@/app/components/PayPal/PayPalButton"
+
 export default function PayPage() {
   return (
-    <div>Pay Page</div>
+    <>
+      <div>Pay Page</div>
+      <PayPalButton amount={65} />
+    </>
   )
 }
