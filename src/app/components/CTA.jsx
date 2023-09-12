91174d27504023bd531a6bd5cbcba3a243229cef
gabewolford <gabewolford@gmail.com>
2023-09-11 21:13:19 -0700

    fixed CTA component, added link

--- src/app/components/CTA.jsx
+++ src/app/components/CTA.jsx
@@ -1,17 +1,17 @@
 import Button from "./Button"
+import Link from "next/link"
 
 export default function CTA({ header, subtext, buttonText, linkTo }) {
   return (
     <section className="flex flex-col gap-4 text-center px-10">
             <h3 className="text-2xl md:text-3xl font-semibold">{header}</h3>
             { subtext && <p>{subtext}</p>}
-            <div className='mx-auto'>
-                <Button
-                    text={buttonText}
-                    linkTo={linkTo}
-                >
-                </Button>
-            </div>
+            <Link 
+              className='mx-auto'
+              href={linkTo}
+            >
+                <Button text={buttonText} />
+            </Link>
     </section>
   )
 }
