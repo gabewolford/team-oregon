a49f37537f5687ca96f566a8c0add792b2606c67
gabewolford <gabewolford@gmail.com>
2023-09-13 17:35:22 -0700

    updated layout

--- src/app/account/page.jsx
+++ src/app/account/page.jsx
@@ -7,13 +7,11 @@ export const metadata = {
   
 export default function AccountPage() {
     return (
-        <>
-            <main className="flex flex-col gap-10 md:gap-20">
-                <div className="mx-6 lg:mx-20 my-4 md:my-10">
-                    <h2 className='text-center text-xl md:text-3xl text-blue-500 font-semibold uppercase'>Account Information</h2>
-                    <AccountInfo />
-                </div>
-            </main>
-        </>
+        <main className="flex flex-col">
+            <div className="mx-6 lg:mx-20 my-4 md:my-10">
+                <h2 className='text-center text-xl md:text-3xl text-blue-500 font-semibold uppercase'>Account Information</h2>
+                <AccountInfo />
+            </div>
+        </main>
     )
 }
