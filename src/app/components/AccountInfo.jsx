7c2f1f5c851a7a1ffa26908e2811787fb29ccc3c
gabewolford <gabewolford@gmail.com>
2023-09-13 17:41:30 -0700

    removed spaces and fragment

--- src/app/components/AccountInfo.jsx
+++ src/app/components/AccountInfo.jsx
@@ -32,61 +32,56 @@ export default function AccountInfo() {
     }
 
     return (
-        <>
-            <div className="flex flex-col gap-4 md:w-1/2 mx-auto my-4 md:my-10">
-                <table>
+        <div className="flex flex-col gap-4 md:w-1/2 mx-auto my-4 md:my-10">
+            <table>
+                <tr>
+                    <td className="text-sm md:text-base font-semibold">First name</td>
+                    <td className="text-sm md:text-base">{firstName}</td>
+                </tr>
+                <tr>
+                    <td className="text-sm md:text-base font-semibold">Last name</td>
+                    <td className="text-sm md:text-base">{lastName}</td>
+                </tr>
+                <tr>
+                    <td className="text-sm md:text-base font-semibold">Email</td>
+                    <td className="text-sm md:text-base">{email}</td>
+                </tr>
+                <tr>
+                    <td className="text-sm md:text-base font-semibold">Account created</td>
+                    <td className="text-sm md:text-base">{accountCreatedDate}</td>
+                </tr>
+                <tr>
+                    <td className="text-sm md:text-base font-semibold">Account status</td>
+                    <td className="text-sm md:text-base">{memberStatus}</td>
+                </tr>
+                {membershipPurchaseDate && 
                     <tr>
-                        <td className="text-sm md:text-base font-semibold">First name</td>
-                        <td className="text-sm md:text-base">{firstName}</td>
-                    </tr>
+                        <td className="text-sm md:text-base font-semibold">Membership start</td>
+                        <td className="text-sm md:text-base">{membershipPurchaseDate}</td>
+                    </tr>}
+                {membershipExpirationDate &&
                     <tr>
-                        <td className="text-sm md:text-base font-semibold">Last name</td>
-                        <td className="text-sm md:text-base">{lastName}</td>
+                        <td className="text-sm md:text-base font-semibold">Membership expiration</td>
+                        <td className="text-sm md:text-base">{membershipExpirationDate}</td>
                     </tr>
-                    <tr>
-                        <td className="text-sm md:text-base font-semibold">Email</td>
-                        <td className="text-sm md:text-base">{email}</td>
-                    </tr>
-                    <tr>
-                        <td className="text-sm md:text-base font-semibold">Account created</td>
-                        <td className="text-sm md:text-base">{accountCreatedDate}</td>
-                    </tr>
-                    <tr>
-                        <td className="text-sm md:text-base font-semibold">Account status</td>
-                        <td className="text-sm md:text-base">{memberStatus}</td>
-                    </tr>
-                    {membershipPurchaseDate && 
-                        <tr>
-                            <td className="text-sm md:text-base font-semibold">Membership start</td>
-                            <td className="text-sm md:text-base">{membershipPurchaseDate}</td>
-                        </tr>}
-                    {membershipExpirationDate &&
-                        <tr>
-                            <td className="text-sm md:text-base font-semibold">Membership expiration</td>
-                            <td className="text-sm md:text-base">{membershipExpirationDate}</td>
-                        </tr>
-                    }
-                </table>
-                {memberStatus === 'Active' ?             
-                <div className="flex flex-col gap-4">
-                    <p className="text-sm md:text-base">Looking to order a new kit? Visit the official <span><a className="font-semibold" href="https://biciclista.us/collections/team-oregon" target="_blank">Team Store</a></span> by Biciclista!</p>
-                    <p className="text-sm md:text-base">Members-only deals from our sponsors <span><Link className="font-semibold" href="/account/sponsor-deals">HERE</Link></span>!</p>
-                </div>
-                :
-                <Link href="/account/pay">
-                    <Button text="Activate membership" />
-                </Link>
                 }
-
-                <button 
-                    onClick={() => signOut()}
-                    className="w-fit flex h-10 px-4 py-2 items-center rounded-full bg-red-500 hover:bg-red-hover text-white-500 font-medium"
-                >Log out
-                </button>
+            </table>
+            {memberStatus === 'Active' ?             
+            <div className="flex flex-col gap-4">
+                <p className="text-sm md:text-base">Looking to order a new kit? Visit the official <span><a className="font-semibold" href="https://biciclista.us/collections/team-oregon" target="_blank">Team Store</a></span> by Biciclista!</p>
+                <p className="text-sm md:text-base">Members-only deals from our sponsors <span><Link className="font-semibold" href="/account/sponsor-deals">HERE</Link></span>!</p>
             </div>
+            :
+            <Link href="/account/pay">
+                <Button text="Activate membership" />
+            </Link>
+            }
 
-
-
-        </>
+            <button 
+                onClick={() => signOut()}
+                className="w-fit flex h-10 px-4 py-2 items-center rounded-full bg-red-500 hover:bg-red-hover text-white-500 font-medium"
+            >Log out
+            </button>
+        </div>
     )
 }
