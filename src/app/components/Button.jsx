2d7dc7aca2255484a066fff6eea41750ef3be9e0
gabewolford <gabewolford@gmail.com>
2023-09-15 15:38:02 -0700

    refactored button to allow for on click prop

--- src/app/components/Button.jsx
+++ src/app/components/Button.jsx
@@ -1,6 +1,6 @@
-export default function Button({ text }) {
+export default function Button({ text, onClick }) {
     return (
-            <button className="flex h-10 px-4 py-2 items-center rounded-full bg-blue-500 hover:bg-blue-hover text-white-500 font-medium">
+            <button className="flex h-10 px-4 py-2 items-center rounded-full bg-blue-500 hover:bg-blue-hover text-white-500 font-medium" onClick={onClick}>
                 {text}
             </button>
     )
