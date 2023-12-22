040790ba45c8512321a0784e183005357c8b67e1
Nix May <nikkimay96@gmail.com>
2023-12-21 16:35:59 -0800

    Add hover to slack join button

--- src/app/components/AccountInfo.jsx
+++ src/app/components/AccountInfo.jsx
@@ -123,7 +123,7 @@ export default function AccountInfo() {
         userData.memberStatus ? (
           <div class="text-center py-4 lg:px-4">
             <a href={slackLink} target="_blank">
-              <div class="p-2 bg-blue-500 items-center text-indigo-100 leading-none lg:rounded-full flex lg:inline-flex" role="alert">
+              <div class="p-2 bg-blue-500 hover:bg-blue-hover items-center text-indigo-100 leading-none lg:rounded-full flex lg:inline-flex" role="button">
                 <Slack color={"#e0e7ff"} />
                 <span class="font-semibold mr-2 ml-4 text-left flex-auto">Join us on Slack</span>
                   <svg class="fill-current opacity-75 h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M12.95 10.707l.707-.707L8 4.343 6.586 5.757 10.828 10l-4.242 4.243L8 15.657l4.95-4.95z"/></svg>
