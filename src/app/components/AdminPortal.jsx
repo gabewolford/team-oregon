71fb414a374701de86b6bfdaecc83ed0d79cd620
gabewolford <gabewolford@gmail.com>
2023-12-23 11:18:15 -0800

    added loading spinner

--- src/app/components/AdminPortal.jsx
+++ src/app/components/AdminPortal.jsx
@@ -3,32 +3,39 @@
 import { getSession } from "next-auth/react";
 import { useState, useEffect } from "react";
 import Button from "./Button";
+import Spinner from "./Spinner";
 
 export default function AdminPortal() {
   const [userListData, setUserListData] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
-  const [itemsPerPage ] = useState(10);
-  const [filterName, setFilterName] = useState('');
+  const [itemsPerPage] = useState(10);
+  const [filterName, setFilterName] = useState("");
+  const [loading, setLoading] = useState(false);
 
   const getUsers = async () => {
-    const session = await getSession();
-    if (session) {
-      const { user } = session;
-      if (user.isAdmin) {
-        const result = await fetch("api/getUsers", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            page: currentPage,
-            itemsPerPage,
-            filterName,
-          }),
-        });
-        const response = await result.json();
-        setUserListData(response.user);
+    try {
+      setLoading(true); // Set loading to true before fetching data
+      const session = await getSession();
+      if (session) {
+        const { user } = session;
+        if (user.isAdmin) {
+          const result = await fetch("api/getUsers", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              page: currentPage,
+              itemsPerPage,
+              filterName,
+            }),
+          });
+          const response = await result.json();
+          setUserListData(response.user);
+        }
       }
+    } finally {
+      setLoading(false);
     }
   };
 
@@ -47,7 +54,7 @@ export default function AdminPortal() {
             <tr key={index}>
               <td>{`${user.firstName} ${user.lastName}`}</td>
               <td>{user.email}</td>
-              <td>
+              <td className="text-center">
                 {user.activeMember ? (
                   <span className="bg-green-600 text-white-500 px-3 py-1 rounded-full">
                     Current
@@ -86,10 +93,16 @@ export default function AdminPortal() {
           id="filter"
           value={filterName}
           onChange={handleFilterChange}
-         className={"border-2 border-blue-500 rounded-lg p-1 w-full"}
+          className={"border-2 border-blue-500 rounded-lg p-1 w-full"}
         />
       </div>
-      {generateTable()}
+      {loading ? (
+        <div className="flex items-center justify-center h-[310px]">
+          <Spinner />
+        </div>
+      ) : (
+        generateTable()
+      )}
       <div className="flex flex-row justify-end items-end">
         <Button
           onClick={() => handlePageChange(currentPage - 1)}
