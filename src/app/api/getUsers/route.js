1baf09b5706e253de84e7fd7e6b2cdd2144c05c7
Nix May <nikkimay96@gmail.com>
2023-12-23 08:58:53 -0800

    Added pagination and search functionality to admin page

--- src/app/api/getUsers/route.js
+++ src/app/api/getUsers/route.js
@@ -2,12 +2,28 @@ import { NextResponse } from "next/server";
 import { connectMongoDB } from "../../../../lib/mongodb";
 import User from "../../../../models/user";
 
-export async function POST() {
+export async function POST(request) {
   try {
     await connectMongoDB();
-    const user = await User.find();
-    return NextResponse.json({ user });
+
+    const { page = 1, itemsPerPage = 10, filterName } = await request.json();
+
+    const query = {};
+    if (filterName) {
+      query.$or = [
+        { firstName: { $regex: filterName, $options: "i" } },
+        { lastName: { $regex: filterName, $options: "i" } },
+      ];
+    }
+
+    const totalUsers = await User.countDocuments(query);
+
+    const users = await User.find(query)
+      .skip((page - 1) * itemsPerPage)
+      .limit(itemsPerPage);
+
+    return NextResponse.json({ user: users, totalUsers });
   } catch (error) {
-    console.log(error);
+    console.error(error);
   }
 }
--- src/app/components/Button.jsx
+++ src/app/components/Button.jsx
@@ -1,6 +1,6 @@
-export default function Button({ text, onClick }) {
+export default function Button({ text, onClick, disabled }) {
     return (
-            <button className="flex h-10 px-4 py-2 items-center rounded-full bg-blue-500 hover:bg-blue-hover text-white-500 font-medium" onClick={onClick}>
+            <button className="flex h-10 px-4 py-2 items-center rounded-full bg-blue-500 hover:bg-blue-hover text-white-500 font-medium" onClick={onClick} disabled={disabled}>
                 {text}
             </button>
     )
