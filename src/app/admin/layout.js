1ee71afa49f6b40b8e2de5ac10110a1695be1367
gabewolford <gabewolford@gmail.com>
2024-03-01 20:07:08 -0800

    increased z index for sanity backend

--- src/app/admin/layout.js
+++ src/app/admin/layout.js
@@ -1,6 +1,6 @@
 export default function Layout({ children }) {
   return (
-    <div className="absolute top-0 left-0 min-h-svh max-h-svh w-full">
+    <div className="absolute top-0 left-0 min-h-svh max-h-svh w-full z-50">
       {children}
     </div>
   );
